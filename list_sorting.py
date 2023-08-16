33a23ebffe8740ac2859f6dac496a7e028094a29
Teja-Ram-Pooniya <71184737+Teja-Ram-Pooniya@users.noreply.github.com>
2023-08-16 10:36:11 +0530

    Create list_sorting.py

---
+++ list_sorting.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 10:01:41 2023
+
+@author: Teja Ram Pooniya
+@Program: List in Python and Sorting That List to Sequencilly 
+"""
+# Generate a list of numbers
+numbers = [5, 2, 8, 1, 9, 3, 6, 4, 7]
+
+# Sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Print the sorted list
+print(sorted_numbers)
