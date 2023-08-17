8251589efc33c49a674ca0d1fa758c87eb1bdaf2
Teja-Ram-Pooniya <71184737+Teja-Ram-Pooniya@users.noreply.github.com>
2023-08-17 22:23:29 +0530

    Create Power-Operator.py
    The power operator allows you to perform exponentiation easily in Python. 
    
    You can use it with both integer and floating-point numbers, as well as with negative exponents to calculate reciprocals.

---
+++ Power-Operator.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 22:17:17 2023
+
+@author: Teja Ram Pooniya
+@Program: Topic - Power Operator in Python
+
+@Description: 
+    the power operator is represented by two asterisks **. 
+    It's used to raise a number to a specified power. 
+    Here's how you can use the power operator.
+"""
+
+# Using the power operator
+result = 2 ** 3  # 2 raised to the power of 3
+print(result)    # Output: 8
+
+# Power operator with float numbers
+float_result = 3.5 ** 2
+print(float_result)  # Output: 12.25
+
+# Power operator with negative exponents
+negative_exponent = 10 ** -2
+print(negative_exponent)  # Output: 0.01
+
+"""
+The power operator allows you to perform exponentiation easily in Python. 
+You can use it with both integer and floating-point numbers, as well as 
+with negative exponents to calculate reciprocals.
+"""
