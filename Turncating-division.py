0de474a12c2082a234b01bc1eca8a97f17cbb956
Teja-Ram-Pooniya <71184737+Teja-Ram-Pooniya@users.noreply.github.com>
2023-08-17 22:17:01 +0530

    Create Turncating-division.py

---
+++ Turncating-division.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 22:11:55 2023
+
+@author: Teja Ram Pooniya
+@Program: Topic - Turncating Division in Python
+@Description: 
+    Truncating division in Python is performed using the double forward slash // operator.
+    It performs division and then truncates the decimal part,
+    giving you the integer quotient of the division operation.
+"""
+
+# Turncating Division
+result = 7 // 3
+
+# Print the Turncating Division Result
+print(result)
+
+
