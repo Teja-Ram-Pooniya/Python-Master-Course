09a852351fa710805fbc68e41ba59e7bf3558047
Teja-Ram-Pooniya <71184737+Teja-Ram-Pooniya@users.noreply.github.com>
2023-08-17 22:09:36 +0530

    Create type-checking.py
    type() checking method used for the all int, float, string, list, dictionary.

---
+++ type-checking.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 22:06:19 2023
+
+@author: Teja Ram Pooniya
+@program: Topic - Type checking in python
+"""
+
+# Example variables of different types
+integer_var = 42
+float_var = 3.14
+string_var = "Hello, Python!"
+list_var = [1, 2, 3]
+dictionary_var = {"name": "Alice", "age": 30}
+
+# Check the data type using type()
+print(type(integer_var))       # Output: <class 'int'>
+print(type(float_var))         # Output: <class 'float'>
+print(type(string_var))        # Output: <class 'str'>
+print(type(list_var))          # Output: <class 'list'>
+print(type(dictionary_var))    # Output: <class 'dict'>
